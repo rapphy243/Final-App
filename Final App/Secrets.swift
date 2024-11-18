003de301730fc7635858d73ba4e37c5ce944f86c
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2024-11-17 18:29:01 -0600

    Man I love secrets

---
+++ Final App/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  secrets.swift
+//  Final App
+//
+//  Created by Raphael Abano on 11/17/24.
+//
+
+//This of course is not a real API key. You should never hardcode your API keys in your code. You should use a .gitignore file to hide your secrets.
+
+enum Secrets {
+    static let unsplashAPIKey = "ThisisnotaRealAPIKey"
+}
